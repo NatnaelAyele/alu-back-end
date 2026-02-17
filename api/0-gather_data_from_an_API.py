b1f28793233b9f47c90446a181cc04fdb134b03f
Natnael Ayele <n.eticha@alustudent.com>
2026-02-17 22:58:20 +0300

    solution to q0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,32 +7,43 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"UsageError: python3 {__file__} employee_id(int)")
-        sys.exit(1)
+def fetch_employee_progress(employee_id):
 
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    user_response = requests.get(f"{base_url}/users/{employee_id}")
-    todos_response = requests.get(
-        f"{base_url}/todos",
-        params={"userId": employee_id}
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id
     )
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        employee_id
+    )
+
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+
+    if user_response.status_code != 200:
+        return
 
     user = user_response.json()
     todos = todos_response.json()
 
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        sys.exit()
-
     employee_name = user.get("name")
     total_tasks = len(todos)
-    done_tasks = [task for task in todos if task["completed"]]
-    total_done_tasks = len(done_tasks)
+    done_tasks = [task for task in todos if task.get("completed")]
+    number_of_done_tasks = len(done_tasks)
 
-    print(f"Employee {employee_name} is done with tasks"
-          f"({total_done_tasks}/{total_tasks}):")
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name,
+        number_of_done_tasks,
+        total_tasks
+    ))
+
     for task in done_tasks:
-        print(f"\t {task['title']}")
+        print("\t {}".format(task.get("title")))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        try:
+            employee_id = int(sys.argv[1])
+            fetch_employee_progress(employee_id)
+        except ValueError:
+            pass
