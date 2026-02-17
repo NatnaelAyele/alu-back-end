bff934e1bb43ce0340b14edbb10e40b8eeaa1ab6
Natnael Ayele <n.eticha@alustudent.com>
2026-02-17 22:00:00 +0300

    pep8 style correction

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-fetch and display names, number of done tasks and names of done tasks for a given user id
+fetch and display names, number of done tasks and names
+of done tasks for a given user id
 """
 import requests
 import sys
@@ -11,18 +12,20 @@ if __name__ == "__main__":
 
     api_url = "https://jsonplaceholder.typicode.com"
     EMPLOYEE_ID = sys.argv[1]
-    response = requests.get( f"{api_url}/users/{EMPLOYEE_ID}/todos",  params={"_expand": "user"})
-    data=response.json()
+    response = requests.get(f"{api_url}/users/{EMPLOYEE_ID}/todos",
+                            params={"_expand": "user"})
+    data = response.json()
 
     if response.status_code != 200:
         print("request not successfull")
         sys.exit()
 
-    employee_name = data[0].get("user",{}).get("name")
+    employee_name = data[0].get("user", {}).get("name")
     total_tasks = len(data)
     done_tasks = [task for task in data if task.get("completed")]
     total_done_tasks = len(done_tasks)
 
-    print(f"Employee {employee_name} is done with tasks({total_done_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is"
+          f" done with tasks({total_done_tasks}/{total_tasks}):")
     for task in done_tasks:
         print(f'\t {task.get("title")}')
