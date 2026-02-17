f35a33802cf83fa8ed94dcf6199eafe2ee403400
Natnael Ayele <n.eticha@alustudent.com>
2026-02-17 21:34:38 +0300

    solution for question 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+api_url = "https://jsonplaceholder.typicode.com"
+EMPLOYEE_ID = sys.argv[1]
+response = requests.get( f"{api_url}/users/{EMPLOYEE_ID}/todos",  params={"_expand": "user"})
+data=response.json()
+
+if response.status_code != 200:
+    print("request not successfull")
+    sys.exit()
+    
+employee_name = data[0].get("user",{}).get("name")
+total_tasks = len(data)
+done_tasks = [task for task in data if task.get("completed")]
+total_done_tasks = len(done_tasks)
+
+print(f"Employee {employee_name} is done with tasks({total_done_tasks}/{total_tasks}):")
+for task in done_tasks:
+    print(f'\t {task.get("title")}')
