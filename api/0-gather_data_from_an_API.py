76fe7e760376c952453e391cf6ec17da617f9380
Natnael Ayele <n.eticha@alustudent.com>
2026-02-17 22:15:29 +0300

    changed formating

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,25 +6,31 @@ of done tasks for a given user id
 import requests
 import sys
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        sys.exit()
+        print(f"UsageError: python3 {__file__} employee_id(int)")
+        sys.exit(1)
 
-    api_url = "https://jsonplaceholder.typicode.com"
+    API_URL = "https://jsonplaceholder.typicode.com"
     EMPLOYEE_ID = sys.argv[1]
-    response = requests.get(f"{api_url}/users/{EMPLOYEE_ID}/todos",
-                            params={"_expand": "user"})
+
+    response = requests.get(
+        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
+        params={"_expand": "user"}
+    )
     data = response.json()
 
-    if response.status_code != 200:
-        print("request not successfull")
-        sys.exit()
+    if not len(data):
+        print("RequestError:", 404)
+        sys.exit(1)
 
     employee_name = data[0]["user"]["name"]
     total_tasks = len(data)
-    done_tasks = [task for task in data if task.get("completed")]
+    done_tasks = [task for task in data if task["completed"]]
     total_done_tasks = len(done_tasks)
-    print(f"Employee {employee_name} is"
-          f" done with tasks({total_done_tasks}/{total_tasks}):")
+
+    print(f"Employee {employee_name} is done with tasks"
+          f"({total_done_tasks}/{total_tasks}):")
     for task in done_tasks:
-        print(f'\t {task.get("title")}')
+        print(f"\t {task['title']}")
